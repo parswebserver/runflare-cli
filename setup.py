3b0f8ef45bb1f051a9a002612f5c63491e632874
unknown <parswebserver@gmail.com>
2021-10-26 15:31:31 +0330

    fix install

--- setup.py
+++ setup.py
@@ -4,17 +4,36 @@ import setuptools
 
 from runflare import VERSION
 
-file_obj = open("requirements.txt","r")
-requirements = file_obj.readlines()
-file_obj.close()
-
 setup(
     name='runflare',
     packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
     version=VERSION,
     description='Official CLI for runflare PaaS',
     author='RUNFLARE DEVELOPMENT TEAM',
-    install_requires=requirements,
+    install_requires=[
+        'certifi==2021.10.8',
+        'charset-normalizer==2.0.7',
+        'colorama==0.4.4',
+        'fire==0.4.0',
+        'halo==0.0.31',
+        'idna==3.2',
+        'log-symbols==0.0.14',
+        'prompt-toolkit==1.0.14',
+        'Pygments==2.10.0',
+        'PyInquirer==1.0.3',
+        'pytz==2021.3',
+        'regex==2021.10.8',
+        'requests==2.26.0',
+        'requests-toolbelt==0.9.1',
+        'six==1.16.0',
+        'spinners==0.0.24',
+        'termcolor==1.1.0',
+        'tzdata==2021.2.post0',
+        'tzlocal==3.0',
+        'urllib3==1.26.7',
+        'wcwidth==0.2.5',
+        'websockets==10.0',
+    ],
     author_email='parswebserver@gmail.com',
     url='https://github.com/parswebserver/runflare',
     keywords=['PaaS', 'runflare','Kubernetes'],
